c9156a605a77ebabc40c917d6684995ab74be331
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2021-08-31 02:35:16 +0000

    wallet login

--- src/App.js
+++ src/App.js
@@ -3,10 +3,48 @@ import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  const [currAccount, setCurrentAccount] = React.useState("");
 
-  const wave = () => {
-    
+  const checkIfWalletConnected = () => {
+    const { ethereum } = window;
+    if (!ethereum){
+      console.log("You need a MetaMask account!");
+      return;
+    } else {
+      console.log("Ethereum object:", ethereum);
+    }
+
+    ethereum.request({method: "eth_accounts"})
+      .then(accounts => {
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found authorized account:", account);
+
+          setCurrentAccount(account);
+        } else {
+          console.log("No authorized account found");
+        }
+      })
   }
+
+  const connectWallet = () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("MetaMask is required to use this site");
+    }
+
+    ethereum.request({method: "eth_requestAccounts"})
+      .then(accounts => {
+        console.log("Connected", accounts[0]);
+        setCurrentAccount(accounts[0]);
+      })
+      .catch(err => console.log(err));
+  }
+
+  React.useEffect(() => {
+    console.log("Test");
+    checkIfWalletConnected();
+  }, [])
   
   return (
     <div className="mainContainer">
@@ -20,9 +58,13 @@ export default function App() {
         Welcome to an auction site. Connect your wallet below to get started
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
+
+        {currAccount ? null : (
+          <button className="button" onClick={connectWallet}>
+          Click me
         </button>
+        )}
+
       </div>
     </div>
   );
