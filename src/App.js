0942d59c62ab912da1af5eddc3cb622b9005b6a3
Tyler Elston <24640479+tylerelston@users.noreply.github.com>
2021-09-01 00:30:21 +0000

    interested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from './utils/auction.json';
 
 export default function App() {
   const [currAccount, setCurrentAccount] = React.useState("");
+  const contractAddress = "0x4657a314ccfbeB47f42D6581e78B2a9D37Acc9C4";
+  const contractABI = abi.abi;
+  let mining = false;
 
   const checkIfWalletConnected = () => {
     const { ethereum } = window;
@@ -41,6 +45,31 @@ export default function App() {
       .catch(err => console.log(err));
   }
 
+  const getTotalInterested = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const auctionContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    let count = await auctionContract.getInterested();
+    console.log("Total interested:", count.toNumber());
+  }
+
+  const interested = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const auctionContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    const interestedTxn = await auctionContract.interested();
+    mining = true;
+    console.log("Mining..", interestedTxn.hash);
+    await interestedTxn.wait();
+    console.log("Mined!", interestedTxn.hash);
+    mining = false;
+
+    let count = await auctionContract.getInterested();
+    console.log("New Total interested:", count.toNumber());
+  }
+
   React.useEffect(() => {
     console.log("Test");
     checkIfWalletConnected();
@@ -62,6 +91,20 @@ export default function App() {
         {currAccount ? null : (
           <button className="button" onClick={connectWallet}>
           Click me
+        </button>
+        )}
+
+        <button className="button" onClick={getTotalInterested}>
+          Total Interested
+        </button>
+
+        <button className="button" onClick={interested}>
+          Interested
+        </button>
+
+        {!mining ? null : (
+          <button className="button" onClick={connectWallet}>
+          <span role="img" aria-label="hourglass">⏳</span>
         </button>
         )}
 
